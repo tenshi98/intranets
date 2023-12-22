939414cf1d80ed753d96ec35f522d7eb570a1814
Victor Reyes <tenshi98@gmail.com>
2023-12-22 18:43:07 -0300

    Eliminacion clase no utilizada
    Eliminacion clase no utilizada

--- sistema_web_clientes_crosstech/gestion_tickets.php
+++ sistema_web_clientes_crosstech/gestion_tickets.php
@@ -91,7 +91,7 @@ $rowdata = mysqli_fetch_assoc ($resultado);
 ?>
 
 <div class="col-xs-12 col-sm-10 col-md-9 col-lg-8 fcenter">
-	<div class="box dark">
+	<div class="box">
 		<header>
 			<div class="icons"><i class="fa fa-edit" aria-hidden="true"></i></div>
 			<h5>Modificacion Ticket</h5>
@@ -134,7 +134,7 @@ $rowdata = mysqli_fetch_assoc ($resultado);
 } elseif(!empty($_GET['new'])){ ?>
 
 <div class="col-xs-12 col-sm-10 col-md-9 col-lg-8 fcenter">
-	<div class="box dark">
+	<div class="box">
 		<header>
 			<div class="icons"><i class="fa fa-edit" aria-hidden="true"></i></div>
 			<h5>Crear Ticket</h5>
